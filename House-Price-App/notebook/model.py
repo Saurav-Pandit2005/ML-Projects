b0a98d8438fded4dac83c18ef52f7238d3fa977a
Saurav Pandit <sauravpandit9691@gmail.com>
2026-05-18 18:44:44 +0530

    change model.py path..

--- House-Price-App/notebook/model.py
+++ House-Price-App/notebook/model.py
@@ -33,5 +33,5 @@ pred = np.expm1(model.predict(X_test))
 print("R2 Score:", r2_score(np.expm1(y_test), pred))
 
 # Save model
-model = joblib.load("../model/model.pkl")
+joblib.dump(model, "model/model.pkl")
 print("Model saved!")
